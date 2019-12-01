db658a2f795868f2af9c39ca2096a2b442f02959
Phillip Tran <ptrandev@protonmail.com>
2019-12-01 12:21:29 -0500

    ultrasonic distance sensor code complete

--- README.md
+++ README.md
@@ -5,10 +5,17 @@ Python code for HC-SR04 distance finder.
 ## Pins
 
 Trigger: Pin 7 (GPIO 7)
+
 Echo: Pin 11 (GPIO 0)
 
 ![GPIO Pins](images/gpio_pins.png)
 
-# Sensors
+## Sensors
 
 ![Sensors Diagram](images/sensors_diagram.png)
+
+## Resources
+
+- [Using a Raspberry Pi distance sensor (ultrasonic sensor HC-SR04)](https://tutorials-raspberrypi.com/raspberry-pi-ultrasonic-sensor-hc-sr04/)
+
+- [Raspberry Pi Distance Sensor using the HC-SR04](https://pimylifeup.com/raspberry-pi-distance-sensor/)
--- hc_sr04_distance/hc-sr04_distance.py
+++ hc_sr04_distance/hc-sr04_distance.py
@@ -1,8 +1,57 @@
-###########################
-# HC-SR04 distance finder #
-###########################
+#!/usr/bin/python
 
 # import libraries
 import RPi.GPIO as GPIO
 import time
 
+# GPIO mode (BOARD / BCM)
+GPIO.setmode(GPIO.BOARD)
+
+# set GPIO pins
+PIN_TRIGGER = 7
+PIN_ECHO = 11
+
+# set GPIO IN/OUT direction
+GPIO.setup(PIN_TRIGGER, GPIO.OUT)
+GPIO.setup(PIN_ECHO, GPIO.IN)
+
+# allow sensor to settle for more accurate readings
+GPIO.output(PIN_TRIGGER, GPIO.LOW)
+print "waiting for sensor to settle"
+time.sleep(2)
+
+# calculates distance
+def distance():
+    # trigger sensor, sending out pulse for 1ns
+    GPIO.output(PIN_TRIGGER, GPIO.HIGH)
+    time.sleep(0.00001)
+
+    # deactivate sensor
+    GPIO.output(PIN_TRIGGER, GPIO.LOW)
+
+    # calculate times for pulse to be sent and receivec
+    while GPIO.input(PIN_ECHO) == 0:
+        pulse_start_time = time.time()
+    while GPIO.input(PIN_ECHO) == 1:
+        pulse_end_time = time.time()
+
+    # find duration of pulse
+    pulse_duration = pulse_end_time - pulse_start_time
+
+    # find distance rounded to 2 decimal places and return value
+    distance = round(pulse_duration * 17150, 2)
+    return distance
+
+if __name__ == "__main__":
+    # continuously run distance calculation every second
+    try:
+        while True:
+            dist = distance()
+            print "Distance:", dist, "cm"
+
+            time.sleep(1)
+        
+    # end program upon keyboard interrupt (ctrl+c)
+    except KeyboardInterrupt:
+        print "stopping measurement"
+        GPIO.cleanup()
